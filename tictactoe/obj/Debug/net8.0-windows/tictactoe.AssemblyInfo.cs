cf95ca3ae14059177b8afe6c9bfc4dd0574be171
Kamila Świercz <01189013@pw.edu.pl>
2025-04-21 13:13:10 +0200

    change gitignore

--- tictactoe/obj/Debug/net8.0-windows/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0-windows/tictactoe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4ef02d901c2f335ef5e472f5613158e4a88666")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b482c17ccdfc09c5f004b663e8a07fea449154e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
